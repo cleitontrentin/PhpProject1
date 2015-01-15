89e5555d0ccf7c837cf72dde000e11c24afa8cfb
cleiton <cleiton@Cleiton>
2015-01-15 08:21:45 -0200

    atualização

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
+	/* Função para redirecionar a navegação*/
+	header("location: view/index.php");	
 
-require_once "banco/conexao.php";
-
-$link = conectar();
-$SQL = "SELECT * FROM funcionario";
-$Requisicao  = mysql_query($SQL);
-while($RF = mysql_fetch_array($Requisicao))
-{
-    echo '<pre>';
-    print_r($RF);
-    echo '</pre>';
-}
-
+	exit;
 ?>
 
 
